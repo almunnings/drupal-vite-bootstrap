b5748c66d10dfe391dae63f98ee6570e3460c511
Al Munnings <al.munnings@gmail.com>
2023-08-14 11:08:55 +1000

    Build assets dist

--- dist/assets/back-top-3c91bb73.js
+++
@@ -1,7 +0,0 @@
-class i{constructor(){this.footer=document.querySelector(".site-footer"),this.footer&&(this.inject(),this.toggle(),window.addEventListener("scroll",this.toggle,{passive:!0}))}inject(){const t=`
-    <div class="back-to-top">
-      <a href="#top" class="btn btn-primary rounded-circle" title="Top of page">
-        <i class="fas fa-chevron-up" aria-hidden="true"></i>
-      </a>
-    </div>
-    `;this.footer.insertAdjacentHTML("beforebegin",t),Drupal.behaviors.fa&&Drupal.behaviors.fa.attach(this.footer.parentNode)}toggle(){const t=window.pageYOffset,o=document.querySelector(".back-to-top"),e=document.querySelector(".site-footer").offsetTop-window.innerHeight+32;o.classList.toggle("show",t>=200||t>=e&&e>0)}}export{i as default};
